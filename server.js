90e7875ae01d2eb32a4e5c43959ce38a4fd51f15
fjgonmir <fjgonmir24@gmail.com>
2021-11-20 00:39:57 +0100

    add first get route

--- server.js
+++ server.js
@@ -3,6 +3,20 @@
 const express = require('express');//import module express
 const app = express();//call constructor express
 
+const mockUserData = [
+    {name:"Mark"},
+    {name:"jill"}
+];
+
+//get method
+app.get('users',function(req,resp){
+    resp.json({
+        success:true,
+        message:"successfully got users. Nice!",
+        users:mockUserData
+    })
+})
+
 app.listen(8000,function(){
     console.log("Server is running");
 })
