7a19d75e9a633dc293a41fbb16a9093978664d0b
fjgonmir <fjgonmir24@gmail.com>
2021-11-20 00:49:52 +0100

    add dinamic get method

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //add server
 
 const express = require('express');//import module express
+const { request } = require('http');
 const app = express();//call constructor express
 
 const mockUserData = [
@@ -9,11 +10,20 @@ const mockUserData = [
 ];
 
 //get method
-app.get('users',function(req,resp){
+app.get('/users',function(req,resp){
     resp.json({
         success:true,
         message:"successfully got users. Nice!",
         users:mockUserData
+    })
+})
+
+//get method with param
+app.get("/users/:id",function(req,res){
+    res.json({
+        success:true,
+        message:"got one user",
+        users:req.params.id
     })
 })
 
